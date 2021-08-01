a17cd3d835c3ccf79848b3510ba1e0bdcba99f24
fjdrensen <62104588+fjdrensen@users.noreply.github.com>
2021-08-01 17:16:36 +0200

    Update GGFastChemConverter.py

--- GGFastChemConverter.py
+++ GGFastChemConverter.py
@@ -5,9 +5,8 @@ from astropy.table import Table
 # This code is meant to convert GGchem-formatted Static_Conc.dat files to FastChem formatted output files
 
 # Reading in data
-file = 'inputfile.dat'
+file = '/home/frank/Documents/MSc Research 1/Code/GGchem/Final Static Conc Files/Static_Conc_TP00.dat'
 data = ascii.read(file, header_start = 2, data_start = 3)
-header = data.colnames
 
 # Reverse to FastChem order. This is useful if you reversed the order of TP_LOW in GGchem for convergence
 #data.reverse()
@@ -22,6 +21,8 @@ data.rename_column('pgas', 'Pbar')
 data.rename_column('mu/amu', 'mu')
 data.rename_column('el', 'e_minus')
 
+header = data.colnames
+
 # Converting values
 t = Table()
 rowtotals = np.zeros(len(data))
@@ -30,14 +31,14 @@ rowtotals = np.zeros(len(data))
 # and end, since this can differ for different input parameters.
 
 # Find index of electrons (from which on we find gas phase columns)
-electrons_ind = header.index('el')
+electrons_ind = header.index('e_minus')
 
 # Find condensate indices
 condensates = []
 for i in range(len(header)):
     if header[i][0] == 'n':
-        condensates.append(header[i])
-
+        condensates.append(header[i])     
+        
 condensates.pop(0) # the first 'n' is nHges, not the 'n' before a condensate so we take that one out
 
 condensates_indices = []
@@ -61,8 +62,8 @@ for i in range(len(supsat)):
     supsat_indices.append(header.index(supsat_i)) # this is our list of condensate indices   
     
 # First convert out of log(10) values for relevant columns
-for i in np.arange(len(header)):
-    col = data[i][:]
+for i in range(len(header)):
+    col = data[header[i]]
     if i < electrons_ind: # no conversion for Tk, n_H or Pbar
         pass
     if i > (electrons_ind - 1) and i < supsat_indices[0]: # from e_minus to the last species before the supersaturation ratios
@@ -71,9 +72,9 @@ for i in np.arange(len(header)):
             power_value = 10**float_value
             col[j] = power_value
             rowtotals[j] += power_value # adding to total number density for the mixing ratios
-    if i > (supsat_indices[-1] - 1) and i < condensates_indices[0]: # no action required for the superstaturation ratios
+    if i > (supsat_indices[0] - 1) and i < condensates_indices[0]: # no action required for the superstaturation ratios
         pass
-    if i > (condensates_indices[-1] - 1) and (i < condensates_indices[-1] + 1): # converting the condensates 
+    if (i > supsat_indices[-1]) and (i < condensates_indices[-1] + 1): # converting the condensates 
         for j in range(len(col)):
             float_value = float(col[j])
             power_value = 10**float_value
@@ -84,18 +85,19 @@ for i in np.arange(len(header)):
         power_value = 10**float_value
         col[j] = power_value
 
-# Converting to mixing ratios, using the total of all number density row sums
+# Converting to mixing ratios, using the total of all number density row sums            
+            
 for i in range(len(header)):
-    col = data[i][:]
+    col = data[header[i]]
     if i < electrons_ind: # Tk, n_H, Pbar
         t.add_column(col)
     if i > (electrons_ind - 1) and i < supsat_indices[0]: # gas species
         for j in range(len(col)):
             col[j] = col[j]/rowtotals[j]
         t.add_column(col)
-    if i > (supsat_indices[-1] - 1) and i < condensates_indices[0]: # supersaturation ratios
+    if i > (supsat_indices[0] - 1) and i < condensates_indices[0]: # supersaturation ratios
         pass
-    if i > (condensates_indices[-1] - 1) and (i < condensates_indices[-1] + 1): # condensates
+    if (i > supsat_indices[-1]) and (i < condensates_indices[-1] + 1): # condensates
         for j in range(len(col)):
             col[j] = col[j]/rowtotals[j]
         t.add_column(col)
@@ -103,5 +105,5 @@ for i in range(len(header)):
         t.add_column(col)
     
 # Writing out to new file
-outputfile = 'outputfile.dat'
+outputfile = 'TP00_mixing.dat'
 ascii.write(t, outputfile, overwrite = True)
